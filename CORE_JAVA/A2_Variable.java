e762a246106e1912970740203fc66b79e4701ac6
Bhababhanjan Panda <bhababhanjan2004panda@gmail.com>
2024-10-23 23:58:18 +0530

    VARIABLES

---
+++ CORE_JAVA/A2_Variable.java
@@ -0,0 +1,16 @@
+package JAVA_PROGRAMMING.CORE_JAVA;
+
+public class A2_Variable {
+    int a;
+    float b;
+    double c;
+    String  d;
+    short e;
+    byte f;
+    boolean g;
+    long  h;
+    //HERE a,b,c,d,e,f,g,h are the variable whose values can be changed further
+    /*int ,float,double,short,byte,boolean,long-->>PRIMITIVE DATA TYPES */
+    /*String, Array, Class, Interface-->> NON-PRIMITIVE  DATA TYPES */
+
+}
