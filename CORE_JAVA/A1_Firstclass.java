42577e4a9cead7f89e72b30f1f7b950578d7cc59
Bhababhanjan Panda <bhababhanjan2004panda@gmail.com>
2024-10-23 23:57:35 +0530

    INTRODUCTION

--- CORE_JAVA/A1_Firstclass.java
+++ CORE_JAVA/A1_Firstclass.java
@@ -1,6 +1,6 @@
 package JAVA_PROGRAMMING.CORE_JAVA;
 
-public class A1_Firstclass {
+public class A1_Firstclass { //Creating a class
     public static void main(String[] args) {    //main is a Function
         System.out.println("Hello, World!");//println-->Prints in a new line 
         System.out.print("Hello World");//print-->Prints in the same line
