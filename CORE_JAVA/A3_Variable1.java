b0710cb36467bb5ad60d6a5c542e46e53f5ec8ca
Bhababhanjan Panda <bhababhanjan2004panda@gmail.com>
2024-10-23 23:58:53 +0530

    VARIABLES_1

---
+++ CORE_JAVA/A3_Variable1.java
@@ -0,0 +1,15 @@
+package JAVA_PROGRAMMING.CORE_JAVA;
+
+public class A3_Variable1 {
+    public static void main(String[] args) {
+        int a=90;
+        int b=10;
+        int sum=a+b;
+        int diff=a-b;
+        int mul=a*b;
+        int div=a/b;
+        int mod=a%b;
+    System.out.println(sum+"\n"+diff+"\n"+mul+"\n"+div+"\n"+mod);
+    
+}
+}
