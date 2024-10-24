233caa22ed56964185a82ef2b8894bfddbbc9534
Bhababhanjan Panda <bhababhanjan2004panda@gmail.com>
2024-10-24 17:14:09 +0530

    CHECK DIVISIBILITY BY 2

--- TASK_JAVA/AA_TASK1.java
+++ TASK_JAVA/AA_TASK1.java
@@ -1,6 +1,6 @@
 package JAVA_PROGRAMMING.TASK_JAVA;
 //Checking a number is divisible by 2 or not (USING FOR LOOP)
-public class A1_TASK1 {
+public class AA_TASK1 {
     public static void main(String[] args) {
         int i;
         for(i=1;i<=10;i++)
@@ -10,7 +10,7 @@ public class A1_TASK1 {
                 System.out.println(i+" IS DIVISIBLE BY 2");
             }
             else{
-                System.out.println(i+" IS NOT DIVISIBLE BY 2");
+                System.out.println(i+" IS NOT DIVISIBLE BY 2!");
             }
         }
     }
