fc1c0ba583a18de99b50b8954c1d2dfba8d67978
Bhababhanjan Panda <bhababhanjan2004panda@gmail.com>
2024-10-24 15:19:27 +0530

    FOR_LOOP

---
+++ CORE_JAVA/A10_For_loop.java
@@ -0,0 +1,10 @@
+package JAVA_PROGRAMMING.CORE_JAVA;
+//Print a statement 3 multiple times using for loop
+public class A10_For_loop {
+    public static void main(String[] args) {
+        for(int i=1;i<3;i++)
+        {
+            System.out.println("HELLO JAVA");
+        }
+    }
+}
