28329c550e377eca63b5f83f8f1119a28cbcee16
Bhababhanjan Panda <bhababhanjan2004panda@gmail.com>
2024-10-24 15:32:12 +0530

    DO_WHILE_LOOP

--- CORE_JAVA/A12_Do_While_Loop.java
+++ CORE_JAVA/A12_Do_While_Loop.java
@@ -3,11 +3,11 @@ package JAVA_PROGRAMMING.CORE_JAVA;
 public class A12_Do_While_Loop {
     
 public static void main(String[] args) {
-    int i = 0;
+    int i = 0; //Initialisation
     do{
-        System.out.println(i);
-        i++;
+        System.out.println(i); //Print statement
+        i++; //Increement/Decreement
     }
-    while(i<=10);
+    while(i<=10);//Condition
 }
 }
