91b56449afc9521e050b867dbcbbac40dfb95fc0
Bhababhanjan Panda <bhababhanjan2004panda@gmail.com>
2024-10-24 15:30:12 +0530

    WHILE_LOOP

--- CORE_JAVA/A11_While_loop.java
+++ CORE_JAVA/A11_While_loop.java
@@ -2,11 +2,11 @@ package JAVA_PROGRAMMING.CORE_JAVA;
 //Printing 0-10 using While loop
 public class A11_While_loop {
     public static void main(String[] args) {
-        int i=0;
-        while(i<=10)
+        int i=0; //Initialisation
+        while(i<=10) //while(Condition)
         {
-            System.out.println(i);
-            i++;
+            System.out.println(i); //Print statement
+            i++; //Increement / Decreement
         }
     }
 }
