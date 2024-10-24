c33b0f4828767c520edb05575c48a8f3dd39731b
Bhababhanjan Panda <bhababhanjan2004panda@gmail.com>
2024-10-24 17:00:06 +0530

    MENU DRIVEN PROGRAM

---
+++ TASK_JAVA/A5_TASK5.java
@@ -0,0 +1,43 @@
+package JAVA_PROGRAMMING.TASK_JAVA;
+//Menu driven program
+import java.util.Scanner;
+public class A5_TASK5 {
+    public static void main(String[] args) {    
+        Scanner sc = new Scanner(System.in);
+        int i;
+        int j;
+        do{
+            System.out.println("Enter number 1 or 0:");
+            i=sc.nextInt();
+            switch(i)
+            {
+                case 1:
+                {
+                System.out.println("Enter your mark:");
+                j=sc.nextInt();
+                if(j>=90)
+                {
+                    System.out.println("This is good!");
+                }
+                else if(j<=89 && j>=60)
+                {
+                    System.out.println("This is also good!!");
+                }
+                else
+                {
+                    System.out.println("This is good as well!!!");
+                }
+                break;
+            }
+            default:
+            {
+            System.out.println("THANK YOU FOR VISITING (:");
+            }
+        }
+           
+           
+        }
+        while(i==1);
+    
+    }
+}
