80be1e62790fa2300cdd9e0846b898c3c884fc53
Bhababhanjan Panda <bhababhanjan2004panda@gmail.com>
2024-10-24 21:52:38 +0530

    REVERSE HALF PYRAMID

---
+++ TASK_JAVA/AD_TASK9.java
@@ -0,0 +1,18 @@
+package JAVA_PROGRAMMING.TASK_JAVA;
+//REVRSE HALF PYRAMID
+public class AD_TASK9 {
+    public static void main(String[] args) {
+        //OUTER LOOP
+        for(int i=4;i>=1;i--)
+        {
+            //INNER LOOP
+            for(int j=1;j<=i;j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        
+    }
+}
