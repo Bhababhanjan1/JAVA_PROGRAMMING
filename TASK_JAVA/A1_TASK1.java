abd4202c5e6c4d13e0391aca025d50d36dd769df
Bhababhanjan Panda <bhababhanjan2004panda@gmail.com>
2024-10-24 15:41:49 +0530

    NUMBER DIVISIBLE BY 2 OR NOT (USING FOR LOOP)

---
+++ TASK_JAVA/A1_TASK1.java
@@ -0,0 +1,17 @@
+package JAVA_PROGRAMMING.TASK_JAVA;
+//Checking a number is divisible by 2 or not (USING FOR LOOP)
+public class A1_TASK1 {
+    public static void main(String[] args) {
+        int i;
+        for(i=1;i<=10;i++)
+        {
+            if(i%2==0)
+            {
+                System.out.println(i+" IS DIVISIBLE BY 2");
+            }
+            else{
+                System.out.println(i+" IS NOT DIVISIBLE BY 2");
+            }
+        }
+    }
+}
