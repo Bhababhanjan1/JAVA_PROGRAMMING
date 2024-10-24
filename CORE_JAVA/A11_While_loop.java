ba0420203dec604eac1313d83b11c1da5ee1f3fa
Bhababhanjan Panda <bhababhanjan2004panda@gmail.com>
2024-10-24 15:20:45 +0530

    WHILE_LOOP

---
+++ CORE_JAVA/A11_While_loop.java
@@ -0,0 +1,12 @@
+package JAVA_PROGRAMMING.CORE_JAVA;
+//Printing 0-10 using While loop
+public class A11_While_loop {
+    public static void main(String[] args) {
+        int i=0;
+        while(i<=10)
+        {
+            System.out.println(i);
+            i++;
+        }
+    }
+}
