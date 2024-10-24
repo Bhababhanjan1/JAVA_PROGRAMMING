b00bf7b792e85e21eec3c9932a63b18edc4c192d
Bhababhanjan Panda <bhababhanjan2004panda@gmail.com>
2024-10-24 20:10:18 +0530

    PATTERN-1

---
+++ TASK_JAVA/AD_TASK6.java
@@ -0,0 +1,16 @@
+package JAVA_PROGRAMMING.TASK_JAVA;
+//PATERN PRINTING 
+import  java.util.Scanner;
+
+public class AD_TASK6 {
+    public static void main(String[] args) {
+        for(int i=1;i<5;i++)
+        {
+            for(int j=1;j<=5;j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
