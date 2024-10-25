a32770fd8ff1c60328621dafb0d2dafe638e5268
Bhababhanjan Panda <bhababhanjan2004panda@gmail.com>
2024-10-25 15:58:11 +0530

    O1-TRIANGLE

---
+++ TASK_JAVA/AE05_TASK14.java
@@ -0,0 +1,26 @@
+package JAVA_PROGRAMMING.TASK_JAVA;
+//PATTERN - 01 TRIANGLE
+public class AE05_TASK14 {
+    public static void main(String[] args) {
+        int number =1;
+        //OUTER  LOOP FOR ROWS
+
+        for(int i=1;i<=5;i++)
+        {
+            //INNER  LOOP FOR COLUMNS
+
+            for(int j=1;j<=i;j++)
+            {
+               if((i+j)%2==0)
+               {
+                System.out.print(" 1");
+               }
+               else{
+                System.out.print(" 0");
+               }
+                
+            }
+            System.out.println();
+        }
+    }
+}
