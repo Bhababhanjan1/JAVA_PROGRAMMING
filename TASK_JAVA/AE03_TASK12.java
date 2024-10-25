970f95986795ba4be622caff64d3e11b1b641cd1
Bhababhanjan Panda <bhababhanjan2004panda@gmail.com>
2024-10-25 15:31:54 +0530

    INVERTED HALF PYRAMID WITH NUMBERS

--- TASK_JAVA/AE03_TASK12.java
+++ TASK_JAVA/AE03_TASK12.java
@@ -2,9 +2,11 @@ package JAVA_PROGRAMMING.TASK_JAVA;
 //PATTERN- INVERTED HALF PYRAMID WITH NUMBERS
 public class AE03_TASK12 {
     public static void main(String[] args) {
-        for(int i=5;i>=1;i--)
+        //OUTER LOOP
+        for(int i=5;i>=1;i--) //for(int i=1;i<=5;i++)
         {
-            for(int j=1;j<=i;j++)
+            //INNER LOOP
+            for(int j=1;j<=i;j++) //for(int j=1;j<=5-i+1)
             {
                 System.out.print(j+" ");
             }
