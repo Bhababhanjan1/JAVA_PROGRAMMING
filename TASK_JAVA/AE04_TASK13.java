99e31c88c21058dfc71be5de5c865d610f19e73f
Bhababhanjan Panda <bhababhanjan2004panda@gmail.com>
2024-10-25 15:48:36 +0530

    FLOYD'S TRIANGLE

--- TASK_JAVA/AE04_TASK13.java
+++ TASK_JAVA/AE04_TASK13.java
@@ -3,12 +3,16 @@ package JAVA_PROGRAMMING.TASK_JAVA;
 public class AE04_TASK13 {
     public static void main(String[] args) {
         int number=1; 
+        //OUTER  LOOP FOR ROWS
+
         for(int i=1;i<=5;i++)
         {
+            //INNER  LOOP FOR COLUMNS
+
             for(int j=1;j<=i;j++)
             {
                 System.out.print(number+" ");
-                number++;
+                number++; //number=number+1;
             }
             System.out.println();
         }
