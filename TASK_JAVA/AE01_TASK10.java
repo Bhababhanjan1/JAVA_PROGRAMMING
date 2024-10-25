000c4144eb44f2a1083e4caf64f043172c258f15
Bhababhanjan Panda <bhababhanjan2004panda@gmail.com>
2024-10-25 15:10:21 +0530

    INVERTED HALF PYRAMID (ROTATED 180')

---
+++ TASK_JAVA/AE01_TASK10.java
@@ -0,0 +1,20 @@
+package JAVA_PROGRAMMING.TASK_JAVA;
+// PATTERN- INVERTED HALF PYRAMID (ROTATED 180')
+public class AE01_TASK10 {
+    public static void main(String[] args) {
+        for(int i=4;i>=1;i--) //for(i=1;i<=4;i++)
+        {
+            for(int j=1;j<i;j++) //for(j=1;j<=n-1;j++) //for(j=1;j<=3;j++)
+            {
+                System.out.print(" ");
+            }
+            for(int j=0;j<=4-i;j++)//for(j=1;j<=i;j++)
+            {
+                System.out.print("*");
+
+            }
+            System.out.println();
+        }
+    
+}
+}
