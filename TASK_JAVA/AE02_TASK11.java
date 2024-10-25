cf26bc28a2d4f53dddd6515a8fb23a2ae33929e8
Bhababhanjan Panda <bhababhanjan2004panda@gmail.com>
2024-10-25 15:14:17 +0530

    HALF PYRAMID WITH NUMBERS

---
+++ TASK_JAVA/AE02_TASK11.java
@@ -0,0 +1,14 @@
+package JAVA_PROGRAMMING.TASK_JAVA;
+//PATTERN- HALF PYRAMID WITH NUMBERS
+public class AE02_TASK11 {
+    public static void main(String[] args) {
+        for(int i=1;i<5;i++)
+        {
+            for(int j=1;j<=i;j++)
+            {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
