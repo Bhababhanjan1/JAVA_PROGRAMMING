422ce533454f09c09349f8bc735d3eea8960c1fa
Bhababhanjan Panda <bhababhanjan2004panda@gmail.com>
2024-10-28 00:11:07 +0530

    FUNCTION TO ADD TWO NUMBERS

--- CORE_JAVA/BA01_Functions1.java
+++ CORE_JAVA/BA01_Functions1.java
@@ -1,7 +1,7 @@
 package JAVA_PROGRAMMING.CORE_JAVA;
 //MAKE A FUNCTION TO ADD 2 NUMBERS AND RETURN THE SUM
 import java.util.Scanner;
-public class BA01_Functions {
+public class BA01_Functions1 {
     public static int sum(int x,int y)
     {
         int result=x+y;
