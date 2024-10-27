075d695513e1b07587b5dba1af7f9da90ee8bebe
Bhababhanjan Panda <bhababhanjan2004panda@gmail.com>
2024-10-27 23:50:10 +0530

    FUNCTION INTRO

---
+++ CORE_JAVA/BA00_Fuction_and_Method.java
@@ -0,0 +1,15 @@
+package JAVA_PROGRAMMING.CORE_JAVA;
+//INTRODUCTION TO FUNCTION
+import java.util.Scanner; //input library
+public class BA00_Fuction_and_Method {
+    public static void PrintMyName(String name)
+    {
+        System.out.println(name);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your name:");
+        String name = sc.nextLine();
+        PrintMyName(name);
+    }
+}
