9930aa6c862942745505776f8229c5165432149a
Bhababhanjan Panda <bhababhanjan2004panda@gmail.com>
2024-10-29 16:11:01 +0530

    ARRAY DEFINING -2

---
+++ CORE_JAVA/BA03_Array2.java
@@ -0,0 +1,11 @@
+package JAVA_PROGRAMMING.CORE_JAVA;
+//ARRAY -2
+public class BA03_Array2 {
+    public static void main(String[] args) {
+        int marks[]={0,1,2,3};
+        for(int i=0;i<4;i++)
+        {
+        System.out.println(marks[i]);
+        }
+    }
+}
