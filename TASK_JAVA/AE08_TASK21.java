43400b03c35cb16149c9c442870d70d4a8a4f361
Bhababhanjan Panda <bhababhanjan2004panda@gmail.com>
2024-11-01 23:04:08 +0530

    X-PATTERN

--- TASK_JAVA/AE08_TASK21.java
+++ TASK_JAVA/AE08_TASK21.java
@@ -1,43 +1,68 @@
 package JAVA_PROGRAMMING.TASK_JAVA;
 //PATTERN - X
+/*
+
+*****         *****
+ *****       *****
+   *****    *****
+    *****  *****
+     **********
+    *****  *****
+   *****    *****
+  *****      *****
+ *****        *****
+*****          *****
+
+
+ */
 public class AE08_TASK21 {
     public static void main(String[] args) {
         int i;
         int j;
+        //UPPER PART
         for(i=1;i<=5;i++)
        {
+        //SPACES
         for(j=1;j<=i;j++)
         {
           System.out.print(" ");
         }
+        //STARS
         for(j=1;j<=5;j++)
         {
           System.out.print("*");
         }
+        //SPACES
         for(j=1;j<=5-i;j++)
         {
           System.out.print("  ");
         }
+        //STARS
         for(j=1;j<=5;j++)
         {
           System.out.print("*");
         }
         System.out.println();
        }
+       //LOWER PART
        for(i=1;i<=5;i++)
        {
+        //SPACES
         for(j=1;j<=5-i;j++)
         {
           System.out.print(" ");
         }
+        //STARS
         for(j=1;j<=5;j++)
         {
           System.out.print("*");
         }
+        //SPACES
         for(j=1;j<=i;j++)
         {
           System.out.print("  ");
         }
+        //STARS
         for(j=1;j<=5;j++)
         {
           System.out.print("*");
