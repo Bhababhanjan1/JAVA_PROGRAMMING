422e6cfed7cf24f64cc0596ba7a10af19f992c02
Bhababhanjan Panda <bhababhanjan2004panda@gmail.com>
2024-12-22 08:41:21 +0530

    EXAMPLE

---
+++ CORE_JAVA/A4_Typecasting4.java
@@ -0,0 +1,14 @@
+package JAVA_PROGRAMMING.CORE_JAVA;
+//Example-Typecasting
+public class A4_Typecasting4 {
+    public static void main(String[] args) {
+        byte b=42;
+        char c='a';
+        short s=1024;
+        int i=50000;
+        float f=5.67f;
+        double d=0.1234;
+        double result=(f*b)+(i/c)-(d-s);
+        System.out.println(result);// Result will be in double
+    }
+}
