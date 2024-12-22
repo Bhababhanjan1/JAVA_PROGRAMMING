52c62003956d58f45a7edb52060b82e158981868
Bhababhanjan Panda <bhababhanjan2004panda@gmail.com>
2024-12-22 08:29:20 +0530

    IMPLICIT TYPE CASTING

---
+++ CORE_JAVA/A4_Typecasting1.java
@@ -0,0 +1,12 @@
+package JAVA_PROGRAMMING.CORE_JAVA;
+//Implicit type casting
+public class A4_Typecasting1 {
+    public static void main(String[] args) {
+        int a = 10;
+        float f=a;
+        short s=345;
+        double d=s;
+        System.out.println(f);
+        System.out.println(d); //Coversion of smaller data type into bigger data type
+    }
+}
