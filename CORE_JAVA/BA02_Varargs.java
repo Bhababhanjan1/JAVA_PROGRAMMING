85401ece0f65b775d4d5eb8c17d42b0a350eefb5
Bhababhanjan Panda <bhababhanjan2004panda@gmail.com>
2025-01-04 20:05:41 +0530

    VARIABLE-ARGUMENTS

--- CORE_JAVA/BA02_Varargs.java
+++ CORE_JAVA/BA02_Varargs.java
@@ -6,7 +6,7 @@ public class BA02_Varargs {
         sum(7,8,9,5,4);      
     }
     static int sum(int...a) {
-        System.out.println(Arrays.toString(a));
+        System.out.println(Arrays.toString(a)); //Arrays.toString() is used to convert array to string
        return Arrays.stream(a).sum();
         
 }
