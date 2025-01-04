e23a9ccb6582404553c02e55f23f720def2574fa
Bhababhanjan Panda <bhababhanjan2004panda@gmail.com>
2025-01-04 20:07:46 +0530

    VARIABLE ARGUMENTS

---
+++ CORE_JAVA/BA02_Methods3.java
@@ -0,0 +1,5 @@
+package JAVA_PROGRAMMING.CORE_JAVA;
+//METHOD OVERLOADING
+public class BA02_Methods3 {
+    
+}
--- CORE_JAVA/BA02_Varargs1.java
+++ CORE_JAVA/BA02_Varargs1.java
@@ -1,18 +1,15 @@
 package JAVA_PROGRAMMING.CORE_JAVA;
 //Variable arguments of different data types
 import java.util.Arrays;
-
-
-
 public class BA02_Varargs1 {
     public static void main(String[] args) {
         arr(2,4,'a','b');
 
     }
-    static int arr(int a,int b,char... v)
+    static int arr(int a,int b,char... v) //The variable arguments must always come at last
     {
         System.out.println(a+" "+b);
-        System.out.println(Arrays.toString(v));
+        System.out.println(Arrays.toString(v)); //Arrays.toString() is used to convert array to string
         return a;
     }
 }
