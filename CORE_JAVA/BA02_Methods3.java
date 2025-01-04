c52167644c1248740a28f86c65c639e378ab9644
Bhababhanjan Panda <bhababhanjan2004panda@gmail.com>
2025-01-04 20:09:58 +0530

    METHOD OVERLOADING

--- CORE_JAVA/BA02_Methods3.java
+++ CORE_JAVA/BA02_Methods3.java
@@ -1,5 +1,16 @@
 package JAVA_PROGRAMMING.CORE_JAVA;
 //METHOD OVERLOADING
 public class BA02_Methods3 {
-    
+    public static void main(String[] args) {
+        sum(101);
+        sum("Bhaba");
+        
+    }
+    static void sum(String name) {
+    System.out.println(name);
+}
+static void sum(int a)
+{
+    System.out.println(a);
+}
 }
