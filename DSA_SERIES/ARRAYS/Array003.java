1574e97e8b52204e59c1316aba79cd65cfcc08f2
Bhababhanjan Panda <bhababhanjan2004panda@gmail.com>
2025-01-06 23:11:16 +0530

    PRINTING ARRAY BY TAKING INPUT FROM USER

---
+++ DSA_SERIES/ARRAYS/Array003.java
@@ -0,0 +1,34 @@
+package JAVA_PROGRAMMING.DSA_SERIES.ARRAYS;
+//Printing a 2D array by taking input from user
+import java.util.*;
+public class Array003 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int i,j;
+        System.out.println("Enter the number of rows of an array:");
+        int b=sc.nextInt();
+        System.out.println("Enter the number odf columns of an array:");
+        int c=sc.nextInt();
+        int a[][]=new int [b][c];
+        for( i=0;i<b;i++)
+        {
+            for( j=0;j<c;j++)
+            {
+               System.out.printf("Enter the [%d][%d] element of the array" ,i,j);
+               System.out.println();
+               
+                a[i][j]=sc.nextInt();
+                
+            }
+        }
+        System.out.println("The array is :");
+        for(i=0;i<b;i++)
+        {
+            for(j=0;j<c;j++)
+            {
+                System.out.print(a[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+}
