d70bb6bfcd8a2db38fff79bd2b6b8fadc689a733
Bhababhanjan Panda <bhababhanjan2004panda@gmail.com>
2025-01-06 08:01:49 +0530

    TAKING INPUT IN AN ARRAY

---
+++ DSA_SERIES/ARRAYS/Array001.java
@@ -0,0 +1,36 @@
+package JAVA_PROGRAMMING.DSA_SERIES.ARRAYS;
+//Taking input in array
+import java.util.Scanner;
+public class Array001 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int i;
+        //TYPE-1
+        int a[]=new int [3]; //Array declaration and initialisation
+        a[0]=1;
+        a[1]=2;
+        a[2]=3;
+        System.out.println("The array a[] is:");
+        //for loop for displaying the output
+        for(i=0;i<a.length;i++)
+        {
+            System.out.println(a[i]);
+        }
+        //TYPE-2
+        int b[]=new int[3]; //Array declaration and initialisation
+        System.out.println("Enter the elements of array b[]");
+        //for loop for taking input
+        for(i=0;i<b.length;i++)
+        {
+            b[i]=sc.nextInt(); //Taking input from user
+        }
+        System.out.println("The array b[] is:");
+        //for loop for printing array / displaying the output
+        for(i=0;i<b.length;i++)
+        {
+            System.out.print(b[i]+" ");
+        }
+
+    }
+    
+}
