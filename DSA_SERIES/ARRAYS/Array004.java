5ffa8dd212ab93c37b24b9d88ad8b6b22a0fe6c9
Bhababhanjan Panda <bhababhanjan2004panda@gmail.com>
2025-01-08 20:22:45 +0530

    PRINTING AN 2D ARRAY BY TAKING INPUT FROM USER

--- DSA_SERIES/ARRAYS/Array004.java
+++ DSA_SERIES/ARRAYS/Array004.java
@@ -6,26 +6,38 @@ import java.util.Scanner;
 public class Array004 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int a[][]=new int[3][2];
+        int b[][]=new int[3][2];
        
-        for(int i=0;i<a.length;i++)
+        for(int i=0;i<b.length;i++)
         {
-            for(int j=0;j<a[i].length;j++)
+            for(int j=0;j<b[i].length;j++)
             {
-                a[i][j]=sc.nextInt();
+                System.out.printf("Enter the [%d][%d] element of tye array",i,j);
+                System.out.println();
+            
+                b[i][j]=sc.nextInt();
             }
         }
-        for(int i=0;i<a.length;i++)
+        for(int i=0;i<b.length;i++)
         {
-            for(int j=0;j<a[i].length;j++)
+            for(int j=0;j<b[i].length;j++)
             {
-                System.out.print(a[i][j]+" ");
+                System.out.print(b[i][j]+" ");
             }
             System.out.println();
         }
-        for(int i=0;i<a.length;i++)
+
+        // TYPE_2 PRINTING
+
+        for(int i=0;i<b.length;i++)
         {
-            System.out.println(Arrays.toString(a[i]));
+            System.out.println(Arrays.toString(b[i]));
+        }
+
+        //TYPE 3 PRINTING
+        for(int a[] :b)
+        {
+            System.out.println(Arrays.toString(a));
         }
     }
     
