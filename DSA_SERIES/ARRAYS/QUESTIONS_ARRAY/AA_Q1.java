0676d47b35304793282f4bf7dee65723982e810c
Bhababhanjan Panda <bhababhanjan2004panda@gmail.com>
2025-01-11 06:01:16 +0530

    SWAPPING ELEMENTS IN A 1D ARRAY

--- DSA_SERIES/ARRAYS/QUESTIONS_ARRAY/AA_Q1.java
+++ DSA_SERIES/ARRAYS/QUESTIONS_ARRAY/AA_Q1.java
@@ -1,5 +1,24 @@
 package JAVA_PROGRAMMING.DSA_SERIES.ARRAYS.QUESTIONS_ARRAY;
-
+// SWAPPING ELEMENTS IN ARRAY
+import java.util.Scanner;
+import java.util.Arrays;
 public class AA_Q1 {
-    
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int[] arr = {1, 2, 3, 4, 5};
+        System.out.println("Enter the position of first element to be swapped :");
+        int a=sc.nextInt();
+        System.out.println("Enter the position of second element to be swapped :");
+        int b=sc.nextInt();
+        swap(arr,a,b);
+        System.out.println("After swapping the array is: ");
+        System.out.println(Arrays.toString(arr));
+
+    }
+    static void swap(int arr[],int a,int b)
+    {
+        int temp=arr[a];
+       arr[a]=arr[b];
+        arr[b]=temp;
+    }
 }
