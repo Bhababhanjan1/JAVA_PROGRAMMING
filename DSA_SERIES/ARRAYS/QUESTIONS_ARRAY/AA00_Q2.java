8b337b0e969996a80f00f6355fac9071b0311928
Bhababhanjan Panda <bhababhanjan2004panda@gmail.com>
2025-01-13 18:53:58 +0530

    MAXIMUM ELEMENT OF AN ARRAY

--- DSA_SERIES/ARRAYS/QUESTIONS_ARRAY/AA00_Q2.java
+++ DSA_SERIES/ARRAYS/QUESTIONS_ARRAY/AA00_Q2.java
@@ -1,5 +1,5 @@
 package JAVA_PROGRAMMING.DSA_SERIES.ARRAYS.QUESTIONS_ARRAY;
-//Maximun element of an array
+//Maximum element of an array
 public class AA00_Q2 {
     public static void main(String[] args) {
         int a[]={1,2,3,4,5,6,7,8,9,10};
