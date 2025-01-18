7de095e4e7ded988a423ad75aff49d6133b920f8
Bhababhanjan Panda <bhababhanjan2004panda@gmail.com>
2025-01-18 08:22:35 +0530

    SEARCHING AND COUNTING THE NUMBERS HAVING EVEN NUMBER OF DIGITS

---
+++ DSA_SERIES/LINEAR_SEARCH/AA00_Linear6.java
@@ -0,0 +1,53 @@
+package JAVA_PROGRAMMING.DSA_SERIES.LINEAR_SEARCH;
+//Find the numbers having even number of digits in an Array using Linear Search
+import java.util.Scanner;
+import java.util.Arrays;
+public class AA00_Linear6 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the size of the Array:");
+        int a=sc.nextInt();
+        int arr[]=new int[a];
+        System.out.println("Enter the elements of the Array:");
+        for(int i=0;i<arr.length;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        System.out.println("The Array is :");
+        System.out.println(Arrays.toString(arr));
+        System.out.println("The number of elements having even number of digits is:");
+        System.out.println(countDigits(arr));
+    }
+    static int countDigits(int arr[]){
+        int count = 0;
+        for(int i=0;i<arr.length;i++){
+        if(even(arr[i]))
+        {
+            count++;
+        }
+    }
+    return count;
+}     
+    //Function to check wheather a number contains even no. of digits or not
+    static boolean even(int num)
+    {
+        int numberofdigits=digits(num);
+        if(numberofdigits % 2==0)
+        {
+            return true;
+        }
+        return false;
+
+    }
+    //Count the number of digits in a number
+    static int digits(int num){
+        int count=0;
+        while(num>0)
+        {
+            count++;
+            num=num/10;
+        }
+        return count;
+    }
+    
+}
