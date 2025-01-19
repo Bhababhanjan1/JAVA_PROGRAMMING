c15fbc8e900df853d5a77bed01e27805dbab37fb
Bhababhanjan Panda <bhababhanjan2004panda@gmail.com>
2025-01-19 09:14:55 +0530

    LEETCODE -FINDING THE NUMBERS HAVING EVEN NUMBER OF DIGITS IN AN ARRAY USING LINEAR SEARCH

--- DSA_SERIES/LINEAR_SEARCH/AA00_Linear6a.java
+++ DSA_SERIES/LINEAR_SEARCH/AA00_Linear6a.java
@@ -1,6 +1,6 @@
 package JAVA_PROGRAMMING.DSA_SERIES.LINEAR_SEARCH;
 //Find the numbers having even number of digits in an Array using Linear Search (OPTIMISED)
-public class AA00Linear6a {
+public class AA00_Linear6a {
     public static void main(String[] args) {
         int arr[]={12,3,0,34,78906,271500,95,713747324,676487,875,9145};
         System.out.println(countDigits(arr));
