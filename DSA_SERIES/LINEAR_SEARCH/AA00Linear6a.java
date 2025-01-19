fe97f316ca465642abff60aebf44967fe2b223be
Bhababhanjan Panda <bhababhanjan2004panda@gmail.com>
2025-01-19 08:46:37 +0530

    FINDING THE NUMBERS HAVING EVEN DIGITS FROM AN ARRAY USING LINEAR SEARCH -OPTIMISED

--- DSA_SERIES/LINEAR_SEARCH/AA00Linear6a.java
+++ DSA_SERIES/LINEAR_SEARCH/AA00Linear6a.java
@@ -3,9 +3,30 @@ package JAVA_PROGRAMMING.DSA_SERIES.LINEAR_SEARCH;
 public class AA00Linear6a {
     public static void main(String[] args) {
         int arr[]={12,3,0,34,78906,271500,95,713747324,676487,875,9145};
-        System.out.println(countdigits(-8765));
+        System.out.println(countDigits(arr));
     }
-    static int countdigits(int num)
+    static int countDigits(int arr[]){
+        int count = 0;
+        for(int i=0;i<arr.length;i++){
+        if(even(arr[i]))
+        {
+            count++;
+        }
+    }
+    return count;
+}     
+    //Function to check wheather a number contains even no. of digits or not
+    static boolean even(int num)
+    {
+        int numberofdigits=digits(num);
+        if(numberofdigits % 2==0)
+        {
+            return true;
+        }
+        return false;
+
+    }
+    static int digits(int num)
     {
         if(num<0)
         {
